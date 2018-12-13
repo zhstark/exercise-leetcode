39477f4a21dfdab8fd42a46a66ac8b83bc95d71e
zhstark <zhstark@icloud.com>
2018-12-12 20:54:59 -0500

    Problem 617, pre order traverse

---
+++ Pre-Order.py
@@ -0,0 +1,36 @@
+
+# -------------------------No 617 -------------------------
+# in Python:
+# `x or y` = `x if x else y` =
+# `x ? x : y` in C++
+#----
+# 另外在递归里先调用本身然后 return 有点不理解
+# -------------------------
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+
+        if t1 and t2:
+            node = TreeNode(t1.val+t2.val)
+            node.left = self.mergeTrees(t1.left, t2.left)
+            node.right = self.mergeTrees(t1.right, t2.right)
+            return node
+
+        else:
+            return t1 or t2
+#         elif t1 and not t2:
+#             return t1
+#         elif t2 and not t1:
+#             return t2
+#         else:
+#             return None
